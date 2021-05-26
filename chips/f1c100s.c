298a80bc1d6906ae2ac7f64c71aba8dce4c19de1
JianjunJiang <8192542@qq.com>
2021-05-26 11:16:29 +0800

    [f1c100s]add reset command support

--- chips/f1c100s.c
+++ chips/f1c100s.c
@@ -2,7 +2,14 @@
 
 static int chip_reset(struct xfel_ctx_t * ctx)
 {
-	return 0;
+	uint32_t val;
+
+	val = R32(0x01c20ca0 + 0x18);
+	val &= ~(0xf << 4);
+	val |= (1 << 4) | (0x1 << 0);
+	W32(0x01c20ca0 + 0x18, val);
+	W32(0x01c20ca0 + 0x10, (0xa57 << 1) | (1 << 0));
+	return 1;
 }
 
 static int chip_sid(struct xfel_ctx_t * ctx, uint32_t * sid)
