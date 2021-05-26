d66fb6a146addb9ac3983216683894dfb86fbf83
JianjunJiang <8192542@qq.com>
2021-05-26 13:51:05 +0800

    [d1]add sid and jtag support for d1 chip

--- chips/d1.c
+++ chips/d1.c
@@ -7,12 +7,41 @@ static int chip_reset(struct xfel_ctx_t * ctx)
 
 static int chip_sid(struct xfel_ctx_t * ctx, uint32_t * sid)
 {
-	return 0;
+	sid[0] = R32(0x03006200 + 0x0);
+	sid[1] = R32(0x03006200 + 0x4);
+	sid[2] = R32(0x03006200 + 0x8);
+	sid[3] = R32(0x03006200 + 0xc);
+	return 1;
 }
 
 static int chip_jtag(struct xfel_ctx_t * ctx)
 {
-	return 0;
+	uint32_t addr;
+	uint32_t val;
+
+	/* Config GPIOF0, GPIOF1, GPIOF3 and GPIOF5 to RISCV-JTAG mode */
+	addr = 0x020000f0 + 0x00;
+	val = R32(addr);
+	val &= ~(0xf << ((0 & 0x7) << 2));
+	val |= ((0x4 & 0xf) << ((0 & 0x7) << 2));
+	W32(addr, val);
+
+	val = R32(addr);
+	val &= ~(0xf << ((1 & 0x7) << 2));
+	val |= ((0x4 & 0xf) << ((1 & 0x7) << 2));
+	W32(addr, val);
+
+	val = R32(addr);
+	val &= ~(0xf << ((3 & 0x7) << 2));
+	val |= ((0x4 & 0xf) << ((3 & 0x7) << 2));
+	W32(addr, val);
+
+	val = R32(addr);
+	val &= ~(0xf << ((5 & 0x7) << 2));
+	val |= ((0x4 & 0xf) << ((5 & 0x7) << 2));
+	W32(addr, val);
+
+	return 1;
 }
 
 static int chip_ddr(struct xfel_ctx_t * ctx, const char * type)
