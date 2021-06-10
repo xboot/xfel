2c171866317cb0589d947a9e5f6c84aefe808af3
Jianjun Jiang <8192542@qq.com>
2021-06-10 23:01:52 +0800

    [v831]update v831 spi payload

--- payloads/v831/v831-spi-init/include/v831/reg-dram.h
+++
@@ -1,7 +0,0 @@
-#ifndef __V831_REG_DRAM_H__
-#define __V831_REG_DRAM_H__
-
-#define V831_DRAM_COM_BASE			(0x04002000)
-#define V831_DRAM_CTL_PHY_BASE		(0x04003000)
-
-#endif /* __V831_REG_DRAM_H__ */
